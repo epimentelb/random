7bfdda55c21b41801027e0fb33f4229452da82a4
epimentelb <epimentel@outlook.com>
2017-11-29 09:04:52 -0500

    Update install1.sh
    bitcoind y mysql en otros servers

--- install1.sh
+++ install1.sh
@@ -2,13 +2,14 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 wget https://gist.githubusercontent.com/scatterp2/3f6b1ae1965de18057a896bedc9a6132/raw/cb230dc8b9cc5dab6da64f7e34cf5e50ae373092/passenger.conf
-wget https://gist.githubusercontent.com/scatterp2/5aab2adb578020f93d0f2146e0aac61b/raw/2b2e5fc7e8a95eea3d4b791217c5d1e5b848cd43/bitcoin.conf
+#wget https://gist.githubusercontent.com/scatterp2/5aab2adb578020f93d0f2146e0aac61b/raw/2b2e5fc7e8a95eea3d4b791217c5d1e5b848cd43/bitcoin.conf
 sudo apt-get remove -y apache2
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev imagemagick gsfonts nodejs nginx-extras redis-server software-properties-common python-software-properties nano dialog vim
-sudo add-apt-repository -y ppa:bitcoin/bitcoin
-sudo apt-get update
-sudo apt-get install -y bitcoind
+#bitcoin daemon
+#sudo add-apt-repository -y ppa:bitcoin/bitcoin
+#sudo apt-get update
+#sudo apt-get install -y bitcoind
 cd
 git clone git://github.com/sstephenson/rbenv.git .rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
@@ -22,22 +23,26 @@ echo "gem: --no-ri --no-rdoc" > ~/.gemrc
 gem install bundler
 rbenv rehash
 read -p "Press [Enter] key to continue..."
-mkdir -p ~/.bitcoin
-cp bitcoin.conf ~/.bitcoin/bitcoin.conf
-dialog --msgbox "enter bitcoind user password settings and save" 10 20
-sudo nano ~/.bitcoin/bitcoin.conf
-echo 'deb http://www.rabbitmq.com/debian/ testing main' | sudo tee /etc/apt/sources.list.d/rabbitmq.list
-wget -O- https://www.rabbitmq.com/rabbitmq-release-signing-key.asc |sudo apt-key add -
-sudo apt-get update
-sudo apt-get install -y rabbitmq-server
-sudo rabbitmq-plugins enable rabbitmq_management
-sudo service rabbitmq-server restart
-wget http://localhost:15672/cli/rabbitmqadmin
-chmod +x rabbitmqadmin
-sudo mv rabbitmqadmin /usr/local/sbin
-sudo add-apt-repository 'deb http://archive.ubuntu.com/ubuntu trusty universe'
-sudo apt-get update
-sudo apt-get install -y mysql-server-5.6 redis-server libmysqlclient-dev
+#Bitcoin daemon conf
+#mkdir -p ~/.bitcoin
+#cp bitcoin.conf ~/.bitcoin/bitcoin.conf
+#dialog --msgbox "enter bitcoind user password settings and save" 10 20
+#sudo nano ~/.bitcoin/bitcoin.conf
+#Rabit inst
+#echo 'deb http://www.rabbitmq.com/debian/ testing main' | sudo tee /etc/apt/sources.list.d/rabbitmq.list
+#wget -O- https://www.rabbitmq.com/rabbitmq-release-signing-key.asc |sudo apt-key add -
+#sudo apt-get update
+#sudo apt-get install -y rabbitmq-server
+#sudo rabbitmq-plugins enable rabbitmq_management
+#sudo service rabbitmq-server restart
+#wget http://localhost:15672/cli/rabbitmqadmin
+#chmod +x rabbitmqadmin
+#sudo mv rabbitmqadmin /usr/local/sbin
+# My sql inst
+#sudo add-apt-repository 'deb http://archive.ubuntu.com/ubuntu trusty universe'
+#sudo apt-get update
+#sudo apt-get install -y mysql-server-5.6 redis-server libmysqlclient-dev
+# 
 sudo apt-get install -y dirmngr gnupg
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 561F9B9CAC40B2F7
 sudo apt-get install -y apt-transport-https ca-certificates
@@ -61,7 +66,7 @@ echo "export RAILS_ENV=production" >> ~/.bashrc
 source ~/.bashrc
 mkdir -p ~/peatio
 #git clone https://github.com/ctubio/Krypto-trading-bot.git ~/K
-git clone https://github.com/scatterp/peatio.git ~/peatio/current
+git clone https://github.com/epimentelb/random.git ~/peatio/current
 #git checkout testing01
 cd ~/peatio/current/
 pwd
